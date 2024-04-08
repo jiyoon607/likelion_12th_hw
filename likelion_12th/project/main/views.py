9c0e94e316acc33eef07f45f27de5d09b932600c
jiyoon607 <6judy611@gmail.com>
2024-04-09 02:16:51 +0900

    Edit Django file

--- likelion_12th/project/main/views.py
+++ likelion_12th/project/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    context = {
+        'element': ['Model', 'Template', 'View'],
+    }
+    return render(request, 'main/mainpage.html', context)
+
+def secondpage(request):
+    context = {
+        'number': 6,
+        'members': ['민경', '유진', '서현', '세라', '태준', '수용', '지윤'],
+    }
+    return render(request, 'main/secondpage.html', context)
