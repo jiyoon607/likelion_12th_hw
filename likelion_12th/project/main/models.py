d675b3850174c5cbfb3eb9478f7064c3c1aa425d
jiyoon607 <6judy611@gmail.com>
2024-05-15 18:49:12 +0900

    Create HashTag

--- likelion_12th/project/main/models.py
+++ likelion_12th/project/main/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=30, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    writer = models.CharField(max_length=50)
+    writer = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
     post_type = models.CharField(max_length=50, null=True)
     image = models.ImageField(upload_to="post/", blank=True, null=True)
     body = models.TextField()
     pub_date = models.DateTimeField()
+    tag = models.ManyToManyField(Tag, related_name='post', blank=True)
 
     def __str__(self):
         return self.title
@@ -15,3 +23,12 @@ class Post(models.Model):
     def summary(self):
         return self.body[:20]
 
+class Comment(models.Model):
+    content = models.TextField()
+    pub_date = models.DateTimeField()
+    writer = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, null=False, blank=False, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag, related_name='comment', blank=True)
+
+    def __str__(self):
+        return self.post.title + " : " + self.content[:20] + " by " + self.writer.username
--- likelion_12th/project/main/views.py
+++ likelion_12th/project/main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
-
-from .models import Post
+import re
+from .models import Post, Comment, Tag
 from accounts.models import Profile
 from django.contrib.auth.models import User
 
@@ -22,21 +22,64 @@ def new_post(request):
 
 def detail(request, id):
     post = get_object_or_404(Post, pk=id)
-    return render(request, 'main/detail.html', {'post':post})
+    if request.method == 'GET':
+        comments = Comment.objects.filter(post=post)
+        return render(request, 'main/detail.html', {'post':post, 'comments':comments})
+
+    elif request.method == 'POST':
+        new_comment = Comment()
+
+        new_comment.post = post
+        new_comment.writer = request.user
+        new_comment.content = request.POST['content']
+        new_comment.pub_date = timezone.now()
+
+        new_comment.save()
+
+        #댓글 해시태그 만들기
+        words = re.split(r'[\s]', new_comment.content)
+        tag_list = []
+
+        for w in words:
+            if len(w)>1:
+                if w[0] == '#':
+                        tag_list.append(w[1:])
+
+        for t in tag_list:
+            tag, boolean = Tag.objects.get_or_create(name=t)
+            new_comment.tag.add(tag.id)
+
+        return redirect('main:detail', post.id)
 
 def create(request):
-    new_post = Post()
+    if request.user.is_authenticated:
+        new_post = Post()
 
-    new_post.title = request.POST['title']
-    new_post.writer = request.user
-    new_post.post_type = request.POST['post_type']
-    new_post.image = request.FILES.get('image')
-    new_post.body = request.POST['body']
-    new_post.pub_date = timezone.now()
+        new_post.title = request.POST['title']
+        new_post.writer = request.user
+        new_post.post_type = request.POST['post_type']
+        new_post.image = request.FILES.get('image')
+        new_post.body = request.POST['body']
+        new_post.pub_date = timezone.now()
 
-    new_post.save()
+        new_post.save()
 
-    return redirect('main:detail', new_post.id)
+        words = re.split(r'[\s]', new_post.body) #기존 split함수가 공백 오류나서 수정함
+        tag_list = []
+
+        for w in words:
+            if len(w)>1:
+                if w[0] == '#':
+                        tag_list.append(w[1:])
+
+        for t in tag_list:
+            tag, boolean = Tag.objects.get_or_create(name=t)
+            new_post.tag.add(tag.id)
+
+        return redirect('main:detail', new_post.id)
+    
+    else:
+        return redirect('accounts:login')
 
 def edit(request, id):
     edit_post = Post.objects.get(pk=id)
@@ -44,17 +87,34 @@ def edit(request, id):
 
 def update(request, id):
     update_post = Post.objects.get(pk=id)
+    if request.user.is_authenticated and request.user == update_post.writer:
+        update_post.title = request.POST['title']
+        update_post.post_type = request.POST['post_type']
+        update_post.body = request.POST['body']
+        update_post.pub_date = timezone.now()
 
-    update_post.title = request.POST['title']
-    update_post.writer = request.user
-    update_post.post_type = request.POST['post_type']
-    update_post.image = request.FILES.get('image')
-    update_post.body = request.POST['body']
-    update_post.pub_date = timezone.now()
+        if request.FILES.get('image'):
+            update_post.image = request.FILES['image']
 
-    update_post.save()
+        update_post.tag.clear()
 
-    return redirect('main:detail', update_post.id)
+        words = re.split(r'[\s]', update_post.body) #기존 split함수가 공백 오류나서 수정함
+        tag_list = []
+
+        for w in words:
+            if len(w)>1:
+                if w[0] == '#':
+                        tag_list.append(w[1:])
+
+        for t in tag_list:
+            tag, boolean = Tag.objects.get_or_create(name=t)
+            update_post.tag.add(tag.id)
+
+
+        update_post.save()
+
+        return redirect('main:detail', update_post.id)
+    return redirect('accounts:login', update_post.id)
 
 def delete(request, id):
     delete_post = Post.objects.get(pk=id)
@@ -62,3 +122,24 @@ def delete(request, id):
 
     return redirect('main:secondpage')
 
+def deleteComment(request, id):
+    delete_comment = Comment.objects.get(pk=id)
+    post = Post.objects.get(pk=delete_comment.post.id) #댓글이 달린 post의 id를 가져오기 위함, redirect하기 위함
+    delete_comment.delete()
+
+    return redirect('main:detail', post.id) 
+
+def tag_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'main/tag-list.html', {'tags':tags})
+
+def tag_posts(request, tag_id):
+    tag = get_object_or_404(Tag, id=tag_id)
+    comments = tag.comment.all()
+    posts = []
+    for c in comments:
+        posts += Post.objects.filter(id=c.post_id)
+    posts += tag.post.all()
+    temp_posts = set(posts) #중복제거
+    posts = list(temp_posts)
+    return render(request, 'main/tag-post.html', {'tag':tag, 'posts':posts})
